a4657f581f65a54b0a1c597d4276906bb38b2259
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-06-24 21:24:28 -0300

    feat: add SWR for data fetching and update App component to display system status

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import useSWR from "swr";
+import "./App.css";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+const fetchAPI = async (url: string) => {
+  const response = await fetch(url);
+  const responseBody = await response.json();
+  return responseBody;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  return (
+    <>
+      <h1>Informações do Sistema</h1>
+      <div className="card">
+        <UpdatedAt />
+      </div>
+    </>
+  );
+}
+
+const UpdatedAt = () => {
+  const { isLoading, data } = useSWR(`${apiUrl}/api/v1/status`, fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let updatedAtText = "Carregando...";
+
+  if (!isLoading && data) {
+    updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>Última atualização: {updatedAtText}</div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
